3c8752becdf07eeec1edb9e3d8c9ab4c1d8c7b23
fromjkeee16 <fromjkeee16@gmail.com>
2025-03-31 17:41:18 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,82 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 'use strict';
 
-// write code here
+const MAX_ROWS = 10;
+const MAX_COLS = 10;
+const MIN_ROWS = 2;
+const MIN_COLS = 2;
+
+const addRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+const addColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
+
+addRowButton.addEventListener('click', () => handleClick(addRow));
+addColumnButton.addEventListener('click', () => handleClick(addCol));
+removeRowButton.addEventListener('click', () => handleClick(removeRow));
+removeColumnButton.addEventListener('click', () => handleClick(removeCol));
+
+// no explicit tbody in index.html specified...
+const field = document.querySelector('table.field tbody');
+
+let currentNumRows = field.rows.length || 0;
+let currentNumCols = field.rows[0]?.cells.length || 0;
+
+function handleClick(callback) {
+  callback();
+  updateButtons();
+}
+
+function updateButtons() {
+  addRowButton.disabled = currentNumRows >= MAX_ROWS;
+  addColumnButton.disabled = currentNumCols >= MAX_COLS;
+  removeRowButton.disabled = currentNumRows <= MIN_ROWS;
+  removeColumnButton.disabled = currentNumCols <= MIN_COLS;
+}
+
+function addRow() {
+  if (currentNumRows >= MAX_ROWS) {
+    return;
+  }
+
+  const lastRow = field.rows[currentNumRows - 1].cloneNode(true);
+
+  field.append(lastRow);
+  currentNumRows++;
+}
+
+function removeRow() {
+  const lastRow = field.rows[currentNumRows - 1];
+
+  lastRow.remove();
+  currentNumRows--;
+}
+
+function addCol() {
+  if (currentNumCols >= MAX_COLS) {
+    return;
+  }
+
+  const cell = field.rows[currentNumRows - 1].cells[currentNumCols - 1];
+
+  [...field.rows].forEach((row) => {
+    const newCell = cell.cloneNode(true);
+
+    row.appendChild(newCell);
+  });
+  currentNumCols++;
+}
+
+function removeCol() {
+  const lastColCells = [...field.rows].map(
+    (row) => row.cells[currentNumCols - 1],
+  );
+
+  lastColCells.forEach((cell) => {
+    cell.remove();
+  });
+
+  currentNumCols--;
+}
